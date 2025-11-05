0acf7101276a4826c99489e7a7b69dac52d8e10b
Isabel <2913343@alu.murciaeduca.es>
2025-11-05 09:45:40 +0100

    ejercicioRutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
+});
+
+Route::get('/login', function() {
+    return "Login usuario";
+});
+
+Route::get('/logout', function() {
+    return "Logout usuario";
+});
+
+Route::prefix('familia-profesional')->group(function() {
+    Route::get('/', function() {
+        return "Listado familia profesional";
+    });
+
+    Route::get('/show/{id}', function($id) {
+        return "Vista detalle familia profesional " . $id;
+    });
+
+    Route::get('/create', function() {
+       return "Añadir familia profesional";
+    });
+
+    Route::get('/edit/{id}', function($id) {
+       return "Modificar familia profesional " . $id;
+    });
+});
+
+Route::get('/perfil/{id}', function($id) {
+    return "Visualizar el usuario de " . $id;
 });
