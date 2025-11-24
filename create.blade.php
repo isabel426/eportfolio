dabcf2659b01066ca83a18323b4e59bf4f40a5ef
Enrique Carrascosa <5785235@alu.murciaeduca.es>
2025-11-24 21:14:24 +0100

    Refactor para usar FamiliasProfesionalesController

--- create.blade.php
+++ create.blade.php
@@ -13,32 +13,20 @@
                 </div>
                 <div class="card-body" style="padding:30px">
 
-                    <form action="{{ action([App\Http\Controllers\ProyectosController::class, 'store']) }}" method="POST">
+                    <form action="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'store']) }}" method="POST">
 
                         @csrf
-
+                        
                         <div class="form-group">
                             <label for="nombre">Nombre</label>
                             <input type="text" name="nombre" id="nombre" class="form-control">
                         </div>
 
                         <div class="form-group">
-                            <label for="docente_id">Docente</label>
-                            <input type="number" name="docente_id" id="docente_id">
+                            <label for="docente_id">Codigo</label>
+                            <input type="number" name="codigo" id="codigo">
                         </div>
 
-                        <div class="form-group">
-                            <label for="dominio">Dominio</label><br />
-                            https://github.com/2DAW-CarlosIII/
-                            <input type="text" name="dominio" id="dominio" class="form-control">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="metadatos">Metadatos</label>
-                            <textarea name="metadatos" id="metadatos" class="form-control" rows="3"></textarea>
-                            <br /><small>Cada metadato irá separado del siguiente por una línea <br />
-                                y la clave irá separada por : del valor</small>
-                        </div>
 
                         <div class="form-group text-center">
                             <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
